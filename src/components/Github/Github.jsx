a2c96ecd46988e0cf73b208e989d20500cbea754
Tejas Mudholkar <tejas.mudholkar@outlook.in>
2024-04-06 20:15:59 +0530

    The unused imports have been removed. (#1)

--- src/components/Github/Github.jsx
+++ src/components/Github/Github.jsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from "react";
-import useApiRequest from "../../useApiRequest";
+import React from "react";
 import { useLoaderData } from "react-router-dom";
 
 function Github() {
